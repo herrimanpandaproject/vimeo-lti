2dd8f048d3b5349437c6c88ff0afa0603700b72a
Jaden Hoenes <jadhoenes@gmail.com>
2019-05-03 17:27:02 -0600

    added border to see actual lti to work with; added close button;

--- client/components/App.js
+++ client/components/App.js
@@ -1,6 +1,21 @@
-import React from "react";
-import {Heading} from "@instructure/ui-elements";
+import React from 'react'
+import {Heading} from '@instructure/ui-elements'
+import {CloseButton} from '@instructure/ui-buttons'
 
-export default function (props) {
-  return <Heading as="h1">Basic LTI App</Heading>;
+export default function(props) {
+  return (
+    <div style={{backgroundColor: 'black', width: '1000px', height: '1000px'}}>
+      <div
+        style={{
+          backgroundColor: 'white',
+          margin: 5,
+          position: 'relative',
+          top: 5,
+        }}
+      >
+        <Heading as="h1">Basic LTI App</Heading>
+        <CloseButton />
+      </div>
+    </div>
+  )
 }
